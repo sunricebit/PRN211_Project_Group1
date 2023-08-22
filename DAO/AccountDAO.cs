8b03adb674382e31f2b405e3d6e8911d7644c45b
sunricebit <vuducduynb2002@gmail.com>
2023-08-22 16:56:02 +0700

    fix clean

---
+++ DAO/AccountDAO.cs
@@ -0,0 +1,56 @@
+﻿using PRN211_Project_Group1.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN211_Project_Group1.DAO
+{
+    public class AccountDao
+    {
+        private static readonly Lazy<AccountDao> instance = new Lazy<AccountDao>(() => new AccountDao());
+        public static AccountDao Instance => instance.Value;
+
+        private AccountDao() { }
+
+        public Account GetAccountById(int id)
+        {
+            InteriorProductManagementContext context = new InteriorProductManagementContext();
+            return context.Accounts.FirstOrDefault(item => item.Id == id);
+        }
+
+        public Account GetAccountByEmail(string email)
+        {
+            InteriorProductManagementContext ctx = new InteriorProductManagementContext();
+            return ctx.Accounts.FirstOrDefault(item => item.Email.Equals(email));
+        }
+
+        public IEnumerable<Account> GetAllAccount()
+        {
+            InteriorProductManagementContext context = new InteriorProductManagementContext();
+            return context.Accounts;
+        }
+
+        public void CreateAccount(Account account)
+        {
+            InteriorProductManagementContext context = new InteriorProductManagementContext();
+            context.Accounts.Add(account);
+            context.SaveChanges();
+        }
+
+        public void UpdateAccount(Account account)
+        {
+            InteriorProductManagementContext context = new InteriorProductManagementContext();
+            context.Accounts.Update(account);
+            context.SaveChanges();
+        }
+
+        public void DeleteAccount(Account account)
+        {
+            InteriorProductManagementContext context = new InteriorProductManagementContext();
+            context.Accounts.Remove(account);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DAO/ProductDao.cs
@@ -0,0 +1,62 @@
+﻿using PRN211_Project_Group1.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN211_Project_Group1.DAO
+{
+    public class ProductDao
+    {
+        private static readonly Lazy<ProductDao> instance = new Lazy<ProductDao>(() => new ProductDao());
+        public static ProductDao Instance => instance.Value;
+
+        private ProductDao() { }
+
+        public Product GetProductById(int id)
+        {
+            InteriorProductManagementContext context = new InteriorProductManagementContext();
+            return context.Products.FirstOrDefault(item => item.Id == id);
+        }
+
+        public Product GetProductByName(string name)
+        {
+            InteriorProductManagementContext ctx = new InteriorProductManagementContext();
+            return ctx.Products.FirstOrDefault(item => item.ProductName.Equals(name));
+        }
+
+        public IEnumerable<Product> GetListProductByName(string name)
+        {
+            InteriorProductManagementContext context = new InteriorProductManagementContext();
+            return context.Products.Where(item => item.ProductName.Contains(name));
+        }
+
+        public IEnumerable<Product> GetAllProduct()
+        {
+            InteriorProductManagementContext context = new InteriorProductManagementContext();
+            return context.Products;
+        }
+
+        public void CreateProduct(Product product)
+        {
+            InteriorProductManagementContext context = new InteriorProductManagementContext();
+            context.Products.Add(product);
+            context.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            InteriorProductManagementContext context = new InteriorProductManagementContext();
+            context.Products.Update(product);
+            context.SaveChanges();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            InteriorProductManagementContext context = new InteriorProductManagementContext();
+            context.Products.Remove(product);
+            context.SaveChanges();
+        }
+    }
+}
